fd6c61263218c3d2aece50cbacfaf970f1dfe30b
limin <ayrat_n@bk.ru>
2024-04-18 13:55:34 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from kivy.app import App
+from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+
+Config.set('graphics', 'width', '540')
+Config.set('graphics', 'height', '960')
+Config.set('graphics', 'resizable', '0')
+
+class MainWindow(Screen):
+
+    def change_button_image(self, button, normal, down):
+        button.background_normal = normal
+        button.background_down = down
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file('interface.kv')
+
+class MyApp(App):
+    def build(self):
+        return kv
+
+if __name__ == '__main__':
+    MyApp().run()
