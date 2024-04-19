59e6e7b09da40d77e0f75f2200cfe5a219335486
limin <ayrat_n@bk.ru>
2024-04-19 17:53:26 +0500

    Добавил файлы и поменял интерфейс

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ from kivy.config import Config
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
+from kivy.core.text import LabelBase
+from kivy.utils import get_color_from_hex
+
+Config.set('graphics', 'width', '540')
+Config.set('graphics', 'height', '960')
+
+# Загрузка шрифта
+LabelBase.register('MyFont', fn_regular='fonts/EpilepsySansBold.ttf')
 
 class MainWindow(Screen):
 
     def change_button_image(self, button, normal, down):
         button.background_normal = normal
         button.background_down = down
+        self.manager.current = 'GameWindow'
+
+
+class GameWindow(Screen):
+    pass
 
 class WindowManager(ScreenManager):
     pass
