edcd10ec984ed5153712a6e67f1d7ae26bacbd2b
limin <ayrat_n@bk.ru>
2024-06-08 18:47:12 +0500

    Добавил файлы и поменял интерфейс

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ from kivy.app import App
 from kivy.config import Config
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty, DictProperty
+
 
 class MainWindow(Screen):
 
     def change_button_image(self, button, normal, down):
         button.background_normal = normal
         button.background_down = down
-        self.manager.current = 'GameWindow'
-
+        if button.name == 'new_game':
+            self.manager.current = 'GameWindow'
 
 class GameWindow(Screen):
-    pass
+
+    options = DictProperty(
+        {"Easy": "Images/buttons/backToMainMenu/button_press.png", "Medium": "Images/buttons/backToMainMenu/button_press.png", "HARD": "Images/buttons/backToMainMenu/button_press.png"}
+    )
 
 class WindowManager(ScreenManager):
     pass
